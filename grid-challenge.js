e57ba9b02cd1db03f91d9d765e433682cc073526
liznomura <lizn@hawaii.edu>
2017-06-19 10:08:17 -1000

    forgot a line in the first goal, fixed that, and finished stretch goal i think?

--- grid-challenge.js
+++ grid-challenge.js
@@ -3,10 +3,22 @@ function gridChallenge(cols, rows, fill) {
   let array = [];
   for (i = 0; i < cols; i++) {
   row = [];
-    for (j = 0; i < rows; j++) {
+    for (j = 0; j < rows; j++) {
       row[j] = fill;
     }
+    array[i] = row;
   }
   return array;
 }
 
+function stretchGoal(existingArray, cols, rows, fill) {
+  let newArray = [];
+  for (i = 0; i < cols; i++) {
+    row = [];
+    for (j = 0; j < rows; j++) {
+      row[j] = fill;
+    }
+    newArray[i] = row;
+  }
+  return newArray.push(existingArray);
+}
